fb0ccc627797e1b9df6b5fc2d64364cf3dc730d7
DLBPointon <damonlbp@hotmail.co.uk>
2024-01-16 09:49:58 +0000

    Replacing run with movie as run is no longer a good value for unique-ness

--- js/pb_tolid_specific.js
+++ js/pb_tolid_specific.js
@@ -16,7 +16,7 @@ function tolid_specify(pacbio_data) {
 
       if (detailed_data == "TRUE"){
         data.forEach((item) => {
-          var key = item[four] + ':' + item['well_label'] + ':' + item['run'] + ':' + item['group']
+          var key = item[four] + ':' + item['well_label'] + ':' + item['movie'] + ':' + item['group']
           //console.log(item["specimen"].split("")[0]);
             // Makes three synced arrays
             // More efficient to make a JS Object but there are more changes coming.
@@ -45,7 +45,7 @@ function tolid_specify(pacbio_data) {
         })
       } else {
         data.forEach((item) => {
-          var key = item[four] + ':' + item['well_label'] + ':' + item['run'] + ':' + item['group']
+          var key = item[four] + ':' + item['well_label'] + ':' + item['movie'] + ':' + item['group']
           //console.log(item["specimen"].split("")[0]);
             // Makes three synced arrays
             // More efficient to make a JS Object but there are more changes coming.
